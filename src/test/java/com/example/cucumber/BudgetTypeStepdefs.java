44ecda3536dafc1ddc7c62964ad9f0817cb959ed
Martyna <martyna.szczekocka@gmail.com>
2021-08-24 23:26:27 +0200

    test in progress

---
+++ src/test/java/com/example/cucumber/BudgetTypeStepdefs.java
@@ -0,0 +1,37 @@
+package com.example.cucumber;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class BudgetTypeStepdefs {
+
+    @Given("User is on Budget Type page")
+    public void userIsOnBudgetTypePage() {
+    }
+
+    @When("User enters following operation details")
+    public void userEntersFollowingOperationDetails(String budgetTypeName) {
+
+    }
+
+    @Then("Budget Type should be added")
+    public void budgetTypeShouldBeAdded() {
+    }
+
+    @When("User click on Budget Type with ID {int}")
+    public void userClickOnBudgetTypeWithID(int arg0) {
+    }
+
+    @Then("Budget Type Details page should be open")
+    public void budgetTypeDetailsPageShouldBeOpen() {
+    }
+
+    @Given("User is on Budget Type Details page")
+    public void userIsOnBudgetTypeDetailsPage() {
+    }
+
+    @Then("Budget Type should be deleted")
+    public void budgetTypeShouldBeDeleted() {
+    }
+}
--- src/test/resources/BudgetType.feature
+++ src/test/resources/BudgetType.feature
@@ -1,5 +1,7 @@
 Feature: Add Budget Type
+  Background:
   Given User is on Budget Type page
+  Scenario:
   When User enters following operation details
     | "Basic Account" |
     | "Savings" |
@@ -7,12 +9,16 @@ Feature: Add Budget Type
   Then Budget Type should be added
 
 Feature: Show Budget Type Details
+  Background:
   Given User is on Budget Type page
+  Scenario:
   When User click on Budget Type with ID 1
   Then Budget Type Details page should be open
 
 Feature: Delete Budget Type
+  Background:
   Given User is on Budget Type Details page
+  Scenario:
   When User picking Budget Type with ID
     | 3 |
   Then Budget Type should be deleted
