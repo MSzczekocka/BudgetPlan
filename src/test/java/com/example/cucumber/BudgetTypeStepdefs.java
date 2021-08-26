0afd3cd6508fa0bdbba09d4497e6115c5dcfea03
Martyna <martyna.szczekocka@gmail.com>
2021-08-26 22:56:43 +0200

    test in progress

--- src/test/java/com/example/cucumber/BudgetTypeStepdefs.java
+++ src/test/java/com/example/cucumber/BudgetTypeStepdefs.java
@@ -4,62 +4,50 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
+import java.sql.Connection;
 import java.util.List;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 
 
+
 public class BudgetTypeStepdefs {
 
     @Given("User is on Budget Type page")
     public void userIsOnBudgetTypePage() {
-
+        Connection connection = budgetType.openConnection();
+        assertNotNull(connection);
     }
 
     @When("User enters following operation details")
     public void userEntersFollowingOperationDetails(List<BudgetType> budgetTypes) {
-        String SQL = "INSERT INTO budgetTypes(budgetTypeName) "
-                + "VALUES(?,?)";
-        try (
-                Connection conn = connect();
-                PreparedStatement statement = conn.prepareStatement(SQL);) {
-            int count = 0;
-
-            for (BudgetType budgetType : budgetTypes) {
-                statement.setString(1, budgetTypes.getBudgetTypeName());
-
-                statement.addBatch();
-                count++;
-                // execute every 100 rows or less
-                if (count % 100 == 0 || count == list.size()) {
-                    statement.executeBatch();
-                }
-            }
-        } catch (SQLException ex) {
-            System.out.println(ex.getMessage());
+        for (BudgetType budgetType: budgetTypes){
+            budgetType.addBudgetType();
         }
     }
 
     @Then("Budget Type should be added")
-    public void budgetTypeShouldBeAdded() {
-        List list = pRepo.findAll();
-        assertThat(list).size().isGreaterThan(0);
+    public void budgetTypeShouldBeAdded(List<BudgetType> budgetTypes) {
+        for (BudgetType budgetType: budgetTypes){
+            assertEquals(budgetType.getName(), budgetType.searchBudgetType(budgetTypeID));
+        }
     }
 
-    @When("User click on Budget Type with ID {int}")
-    public void userClickOnBudgetTypeWithID(int arg0) {
-    }
-
-    @Then("Budget Type Details page should be open")
-    public void budgetTypeDetailsPageShouldBeOpen() {
-    }
 
     @Given("User is on Budget Type Details page")
     public void userIsOnBudgetTypeDetailsPage() {
+        Connection connection = budgetType.openConnection();
+    }
+
+    @When("User picking Budget Type with ID")
+    public void userPickingBudgetTypeWithID(int budgetTypeID) {
+        budgetType.deleteBudgetType(budgetTypeID);
     }
 
     @Then("Budget Type should be deleted")
-    public void budgetTypeShouldBeDeleted() {
+    public void budgetTypeShouldBeDeleted(int budgetTypeID) {
+        assertEquals(budgetType.searchBudgetType(budgetTypeID));
     }
+
 }
--- src/test/resources/BudgetType.feature
+++ src/test/resources/BudgetType.feature
@@ -8,12 +8,6 @@ Feature: Add Budget Type
     | "Savings for an Apartment" |
   Then Budget Type should be added
 
-Feature: Show Budget Type Details
-  Background:
-  Given User is on Budget Type page
-  Scenario:
-  When User click on Budget Type with ID 1
-  Then Budget Type Details page should be open
 
 Feature: Delete Budget Type
   Background:
