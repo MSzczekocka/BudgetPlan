2a84624a4cbad92e239d7c432a061f82a15b7b28
Martyna <martyna.szczekocka@gmail.com>
2021-08-28 20:14:19 +0200

    test for Hello World

--- src/main/java/pl/budgetplan/Main.java
+++ src/main/java/pl/budgetplan/Main.java
@@ -6,6 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+        Main main = new Main();
+        String name = "world";
+        main.HelloWorld(name);
+    }
 
+    public String HelloWorld(String name){
+        return "Hello " + name;
     }
 }
+
