8a6d6af334847d4edb861f9b2a284af536a5b85f
Martyna <martyna.szczekocka@gmail.com>
2021-09-03 23:56:34 +0200

    test for get method for budget_type table

--- src/test/java/com/example/cucumber/BudgetTypeGetStepdefs.java
+++ src/test/java/com/example/cucumber/BudgetTypeGetStepdefs.java
@@ -1,18 +1,29 @@
 package com.example.cucumber;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.restassured.http.ContentType;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+import static org.assertj.core.api.BDDAssertions.then;
 
 public class BudgetTypeGetStepdefs {
-    @Given("User perform operation from {string}")
-    public void userPerformOperationFrom(String url) {
+
+    @Given("User perform GET operation for {string}")
+    public void userPerformGETOperationFor(String url) {
+        given().contentType(ContentType.JSON);
     }
 
-    @Then("User should get")
-    public void userShouldGet() {
-
-
+    @And("User perform GET for the post number {string}")
+    public void userPerformGETForThePostNumber(String idBudgetType) {
+        when().get(String.format("http://localhost:8080/budgettype/%s", idBudgetType));
+        then().body()
     }
 
+    @Then("User should get {string}")
+    public void userShouldGet(String nameBudgetType) {
+    }
 }
--- src/test/resources/BudgetTypeGet.feature
+++ src/test/resources/BudgetTypeGet.feature
@@ -1,7 +1,12 @@
 Feature: Test GET method
-  Scenario: Get dates added to budget_type table
-    Given User perform operation from "http://localhost:8080/budgettypes"
-    Then User should get
-      | 1 | "Basic Accounts"     |
-      | 2 | "Basic Savings"      |
-      | 3 | "Investment Account" |
+  Scenario: Get date with ID = 1 from budget_type table
+    Given User perform GET operation for "/budgettypes"
+    And User perform GET for the post number "1"
+    Then User should get "Basic Accounts"
+
+
+
+
+#      | 1 | "Basic Accounts"     |
+#      | 2 | "Basic Savings"      |
+#      | 3 | "Investment Account" |
